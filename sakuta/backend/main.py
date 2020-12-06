a32ab96be7bcd152c48b5cf6e0671a382e21ea85
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2020-12-06 13:35:13 +0100

    Extend OpenAPI spec

--- sakuta/backend/main.py
+++ sakuta/backend/main.py
@@ -1,7 +1,5 @@
-from typing import List
-
-from fastapi import Depends, FastAPI, HTTPException, Response
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from . import models
 from .database import engine
@@ -14,3 +12,18 @@ app = FastAPI()
 app.include_router(contractors.router, tags=["Contractors"])
 app.include_router(branches.router, tags=["Branches"])
 app.include_router(contacts.router, tags=["Contacts"])
+
+
+def custom_openapi():
+    openapi_schema = get_openapi(
+        title="Sakuta - Backend",
+        version="0.5.1",
+        description="This page contacts the latest auto-generated OpenAPI specification for our contractor management module. 🔥 Enjoy the power of Python FastAPI!",
+        routes=app.routes,
+    )
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
