b89102df73f265049bdac650d39397873091eab7
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-01-18 15:08:17 +0100

    Add database variable

--- backend/database.py
+++ backend/database.py
@@ -10,9 +10,8 @@ load_dotenv()
 # Load database URL from environment variable.
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL:
-    engine = create_engine(DATABASE_URL)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
