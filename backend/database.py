af245b5c68637c49d867f4661ee47c81962733bb
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-01-18 15:05:33 +0100

    Fix database variable missing

--- backend/database.py
+++ backend/database.py
@@ -10,8 +10,9 @@ load_dotenv()
 # Load database URL from environment variable.
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+if DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
